353826183c70c3cc228a29476475532e159a9cfa
victoroki <okiomeriv@gmail.com>
2022-12-12 03:52:44 -0800

    complete

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"programmimg is like building a multilingual puzzle");
+	return (0);
+}
